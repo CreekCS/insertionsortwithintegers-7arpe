48d012794fcdf7d7b863acdb73b4c92e6f775ade
7arpe <7arpeggio@gmail.com>
2024-08-28 22:43:00 +0000

    coded code

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,43 @@
-public class InsSortInt {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
 
+public class InsSortInt {
+    public static void main(String[] args) {
+        try {
+            File file = new File("randInts.txt");
+            Scanner scan = new Scanner(file);
+            ArrayList<String> lis = new ArrayList<String>();
+
+            while (scan.hasNextLine()) {
+                lis.add(scan.nextLine());
+            }
+            scan.close();
+            
+            int[] arr = new int[lis.size()];
+            for (int i = 0; i < lis.size(); i++) {
+                arr[i] = Integer.parseInt(lis.get(i));
+            }
+
+            for (int i = 1; i < arr.length; i++) {
+                int k = arr[i];
+                int p = i;
+                for (int j = i - 1; j >= 0; j--) {
+                    if (arr[j] > k) {
+                        arr[j + 1] = arr[j];
+                        p = j;
+                    }
+                }
+                arr[p] = k;
+            }
+            
+            System.out.println(arr[0]);
+            System.out.println(arr[arr.length - 1]);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
